abb4f8bb19aadd3fa7fda56f5dfcffd37b910f79
Andrew <furich42@gmail.com>
2023-01-16 23:53:48 +0300

    added server and client

---
+++ build-Server-Desktop_Qt_5_12_12_GCC_64bit-Debug/build-Client-Desktop_Qt_5_12_12_GCC_64bit-Debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Client_autogen.dir/ParseCache.txt"
+  "Client_autogen"
+  "CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Client.dir/main.cpp.o"
+  "CMakeFiles/Client.dir/main.cpp.o.d"
+  "CMakeFiles/Client.dir/mainwindow.cpp.o"
+  "CMakeFiles/Client.dir/mainwindow.cpp.o.d"
+  "Client"
+  "Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
