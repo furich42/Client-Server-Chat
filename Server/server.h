cfa226a8aa3596122f02bdc0e94731dd64c9a793
Andrew <furich42@gmail.com>
2023-01-17 23:17:23 +0300

    added h file

--- Server/server.h
+++ Server/server.h
@@ -2,10 +2,34 @@
 #define SERVER_H
 
 
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QVector>
+
+
+class Server : public QTcpServer
+{
+    Q_OBJECT
+
+public:
+    Server();
+    QTcpSocket *socket;
+
+private:
+    QVector<QTcpSocket*> sockets;
+    QByteArray data;
+    void sendToClient();
+
+public slots:
+    //void incomingConnection(qintptr socketDescriptor);
+    //void slotReadyRead();
+
+};
+
+
+
 
 #endif // SERVER_H
 
 
-#include <QTcpServer>
-#include <QTcpSocket>
 
