ae50d9aed685a8237405ad7053ee8dbcd9d995be
Andrew <furich42@gmail.com>
2023-01-19 11:40:11 +0300

    added incCon to source

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,1 +1,21 @@
 #include "server.h"
+
+Server::Server() {
+    if(this->listen(QHostAddress::Any, 2323)) {
+        qDebug() << "start";
+    } else {
+        qDebug() << "error";
+    }
+}
+
+
+void Server::incomingConnection(qintptr socketDescriptor) {
+    socket = new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead, this, &Server::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    sockets.push_back(socket);
+    qDebug() << "client connected" << socketDescriptor;
+}
+
