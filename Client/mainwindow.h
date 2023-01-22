9164872ff8c6aa7ee9dbe5b07460190cd5edb6a6
Andrew <furich42@gmail.com>
2023-01-22 14:04:04 +0300

    starting client

--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QTcpSocket *socket;
+    QByteArray data;
+
 };
 #endif // MAINWINDOW_H
